64a1e853dfd3a28f25982b9eaca49e2a1db46d1d
Anderson Birocchi <anderson.birocchi@gmail.com>
2020-05-10 16:59:26 -0300

    robot_path_decoding refactored and now passes both tests

--- 2020/Round B/robot_path_decoding.py
+++ 2020/Round B/robot_path_decoding.py
@@ -1,59 +1,55 @@
-command_dict = {
-    'N': lambda x,y: [x, y-1],
-    'S': lambda x,y: [x, y+1],
-    'W': lambda x,y: [x-1, y],
-    'E': lambda x,y: [x+1, y]
+from collections import deque
+
+BILLION = 1000000000
+
+command_actions = {
+    'N': lambda x,y: [x, (y-1) % BILLION],
+    'S': lambda x,y: [x, (y+1) % BILLION],
+    'W': lambda x,y: [(x-1) % BILLION, y],
+    'E': lambda x,y: [(x+1) % BILLION, y]
 }
-
-def handle_alpha(alpha, position, i):
-    new_position = command_dict[alpha](position[0], position[1])
-    position[0] = new_position[0]
-    position[1] = new_position[1]
-    return i + 1
-
-def handle_digit(digit, program_code, position, i):
-    original_position = [position[0], position[1]]
-    i_resulted = path_decoding(program_code, position, i+1)
-
-    delta_x = position[0] - original_position[0]
-    delta_y = position[1] - original_position[1]
-    
-    position[0] = (original_position[0] + delta_x * digit) % 1000000000
-    position[1] = (original_position[1] + delta_y * digit) % 1000000000
-
-    return i_resulted
-
-def path_decoding(program_code, position, i):
-    while i < len(program_code):
-        command = program_code[i]
-
-        if command.isalpha():
-            i = handle_alpha(command, position, i)
-        elif command.isdigit():
-            i = handle_digit(int(command), program_code, position, i)
-        elif command == '(':
-            i += 1
-        elif command == ')':
-            i += 1
-            break
-
-    return i
 
 def robot_path_decoding():
     program_code = input()
 
+    positions_stack = deque()
+    multipliers_stack = deque()
+
     position = [0, 0]
-    i = 0
+    multiplier = 1
 
-    path_decoding(program_code, position, i)
+    for i in range(len(program_code)):
+        command = program_code[i]
 
-    # Normalize the final response into the challenge output format
-    final_position = [
-        (position[0] % 1000000000) + 1, 
-        (position[1] % 1000000000) + 1
-    ]
+        if command.isalpha():
+            position = command_actions[command](position[0], position[1])
+
+        elif command.isdigit():
+            positions_stack.append(position)
+            multipliers_stack.append(multiplier)
+
+            position = [0, 0]
+            multiplier = int(command)
+
+        elif command == ')':
+            projected_position = [
+                (position[0] * multiplier) % BILLION,
+                (position[1] * multiplier) % BILLION
+            ]
+
+            last_position = positions_stack.pop()
+
+            position = [
+                (last_position[0] + projected_position[0]) % BILLION,
+                (last_position[1] + projected_position[1]) % BILLION
+            ]
+
+            multiplier = multipliers_stack.pop()
     
-    return final_position
+    position[0] = (position[0] % BILLION) + 1
+    position[1] = (position[1] % BILLION) + 1
+
+    return position
 
 # --- Initial point --- #        
 test_cases = int(input())
