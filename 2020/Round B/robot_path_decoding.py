420616aa718de07bf42bb5695ad22a619490520b
Anderson Birocchi <anderson.birocchi@gmail.com>
2020-05-09 22:02:51 -0300

    Avoidong reprocessing subprograms

--- 2020/Round B/robot_path_decoding.py
+++ 2020/Round B/robot_path_decoding.py
@@ -1,38 +1,27 @@
-test_cases = int(input())
-
 command_dict = {
-    'N': lambda x,y: [x,y-1] if y > 1 else [x,1000000000],
-    'S': lambda x,y: [x,y+1] if y < 1000000000 else [x,1],
-    'W': lambda x,y: [x-1,y] if x > 1 else [1000000000,y],
-    'E': lambda x,y: [x+1,y] if x < 1000000000 else [1,y]
+    'N': lambda x,y: [x, y-1],
+    'S': lambda x,y: [x, y+1],
+    'W': lambda x,y: [x-1, y],
+    'E': lambda x,y: [x+1, y]
 }
 
 def handle_alpha(alpha, position, i):
     new_position = command_dict[alpha](position[0], position[1])
     position[0] = new_position[0]
     position[1] = new_position[1]
-    return i+1
+    return i + 1
 
 def handle_digit(digit, program_code, position, i):
     original_position = [position[0], position[1]]
-    j = path_decoding(program_code, position, i+1)
+    i_resulted = path_decoding(program_code, position, i+1)
 
     delta_x = position[0] - original_position[0]
     delta_y = position[1] - original_position[1]
     
-    new_x = position[0] + delta_x * (digit-1)
-    new_y = position[1] + delta_y * (digit-1)
+    position[0] = (original_position[0] + delta_x * digit) % 1000000000
+    position[1] = (original_position[1] + delta_y * digit) % 1000000000
 
-    new_x_norm = new_x % 1000000000 + int(new_x/1000000000)
-    new_y_norm = new_y % 1000000000 + int(new_y/1000000000)
-
-    new_x_norm = new_x_norm if new_x_norm > 0 else 1000000001 + new_x_norm
-    new_y_norm = new_y_norm if new_y_norm > 0 else 1000000001 + new_y_norm
-
-    position[0] = new_x_norm
-    position[1] = new_y_norm
-
-    return j
+    return i_resulted
 
 def path_decoding(program_code, position, i):
     while i < len(program_code):
@@ -53,13 +42,21 @@ def path_decoding(program_code, position, i):
 def robot_path_decoding():
     program_code = input()
 
-    position = [1, 1]
+    position = [0, 0]
     i = 0
 
-    i = path_decoding(program_code, position, i)
+    path_decoding(program_code, position, i)
+
+    # Normalize the final response into the challenge output format
+    final_position = [
+        (position[0] % 1000000000) + 1, 
+        (position[1] % 1000000000) + 1
+    ]
     
-    return position
-        
+    return final_position
+
+# --- Initial point --- #        
+test_cases = int(input())
 
 for t in range(test_cases):
     final_position = robot_path_decoding()
